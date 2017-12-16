1115a110becc6eebe031f5e3b60ea52e79b81577
Chad <chad.zuniga10@gmail.com>
2017-12-15 21:47:40 -0700

    finished node sessions afternoon assigment

--- src/router.js
+++ src/router.js
@@ -4,11 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import Login from './components/Login/Login';
 import Shop from './components/Shop/Shop';
 import Checkout from './components/Checkout/Checkout';
+import Register from './components/Login/Login';
 
 export default (
   <Switch>
     <Route component={ Login } path="/" exact />
     <Route component={ Shop } path="/shop" />
     <Route component={ Checkout } path="/checkout" />
+    <Route component={ Register } path="/register" />
+
   </Switch>
 )
